b938c09e87e9a2a71263999f54de351353a6d3a5
Matheus Eduardo Spigariol <contato.matheused@gmail.com>
2023-01-24 00:02:32 -0300

    Removendo logs

--- app/Models/Publicacao.php
+++ app/Models/Publicacao.php
@@ -4,16 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Activitylog\Traits\LogsActivity;
 
 class Publicacao extends Model
 {
     use HasFactory;
-    use LogsActivity;
-
-    protected static $logAttributes = ['*'];
-    protected static $logOnlyDirty = true;
-    protected static $logName = 'Log de post';
 
     protected $table = "publicacao";
 
--- app/Models/Users.php
+++ app/Models/Users.php
@@ -5,17 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\LogOptions;
 
 class Users extends Model
 {
     use HasFactory, HasApiTokens;
-    use LogsActivity;
-
-    protected static $logAttributes = ['*'];
-    protected static $logOnlyDirty = true;
-    protected static $logName = 'Log de usuario';
     
     protected $table = 'users';
     protected $primaryKey = 'id';
